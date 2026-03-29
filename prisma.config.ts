5047b4029de8cc71766d6e51aeed8aefe1c59b34
Anna Zoi <zoianna01@gmail.com>
2026-03-29 11:40:00 +0300

    prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from '@prisma/config';
 import * as dotenv from 'dotenv';
 import { join } from 'path';
 
+// Load .env.local for Next.js users
 dotenv.config({ path: join(process.cwd(), '.env.local') });
 
 export default defineConfig({
-	schema: join(process.cwd(), 'prisma', 'schema.prisma'),
 	datasource: {
-		url: process.env.DATABASE_URL!,
+		url: process.env.DATABASE_URL,
 	},
 });
