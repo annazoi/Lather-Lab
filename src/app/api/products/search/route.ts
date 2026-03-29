c467290096873bafb4d8d65896c7fe2fdaa3e4f0
Anna Zoi <zoianna01@gmail.com>
2026-03-29 15:23:18 +0300

    vercel

--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { productService } from '@/services/product.service';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
 	try {
 		const { searchParams } = new URL(req.url);
